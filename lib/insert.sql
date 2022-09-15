987fd3bf9843745095a68fce5975d843f75b965b
Casey Tansey <ctansey12600@outlook.com>
2022-09-15 17:04:21 -0400

    commit tree 0171d0fba1a50094c953a25e7bcf5bf8c396d634

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "black", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "brown", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "white", "mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 3, "M", "black", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 11, "F", "brown", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, "M", "white", "unknown", 0);
